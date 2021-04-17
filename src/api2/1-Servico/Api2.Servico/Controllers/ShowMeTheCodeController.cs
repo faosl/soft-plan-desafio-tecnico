455dfc8e4c3d4a14e889020e9f255d918146405f
faosl <faosl@hotmail.com>
2021-04-17 16:46:09 -0300

    Implementação: endpoint ShowMeTheCode

---
+++ src/api2/1-Servico/Api2.Servico/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api2.Servico.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ShowMeTheCodeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {            
+            return Ok(new { UrlGit = _configuration["UrlGit"] });
+        }
+    }
+}
