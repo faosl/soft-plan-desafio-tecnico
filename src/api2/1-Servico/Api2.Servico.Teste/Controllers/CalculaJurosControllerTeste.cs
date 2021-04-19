c87292301e3161a8b8a8edd9407d5b5714dcf7eb
faosl <faosl@hotmail.com>
2021-04-19 07:59:51 -0300

    Implementação de teste de integração na api2.

---
+++ src/api2/1-Servico/Api2.Servico.Teste/Controllers/CalculaJurosControllerTeste.cs
@@ -0,0 +1,40 @@
+﻿using Api2.Dominio.DTOs.Responses;
+using Api2.Dominio.Entidades;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api2.Servico.Teste.Controllers
+{
+    public class CalculaJurosControllerTeste: IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CalculaJurosControllerTeste(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData(1000,1,1010)]
+        public async Task Quando_calculaJuros(double valorInicial,int tempoEmMeses, double resultado)
+        {
+            var client = _factory.CreateClient();
+
+            var calculoJuros = await client.GetFromJsonAsync<CalculoJurosDTO>($"CalculaJuros?valorInicial={valorInicial}&tempoEmMeses={tempoEmMeses}");
+
+            Assert.NotNull(calculoJuros);
+
+            Assert.Equal(valorInicial, calculoJuros?.ValorInicial);
+            Assert.Equal(tempoEmMeses, calculoJuros?.TempoEmMeses);
+            Assert.Equal(resultado, calculoJuros?.ValorFinal);
+
+        }
+
+    }
+}
---
+++ src/api2/3-Dominio/Api2.Dominio/DTOs/Responses/CalculoJurosDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api2.Dominio.DTOs.Responses
+{
+    public class CalculoJurosDTO
+    {
+        public double ValorInicial { get; set; }
+
+        public double TaxaJuros { get; set; }
+
+        public int TempoEmMeses { get; set; }
+
+        public double ValorFinal { get; set; }
+    }
+}
